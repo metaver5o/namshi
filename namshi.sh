6b05e0583be191a04f3a8147344cefcb40f2e5e6
M Matos <devops@corp.mmatos.com>
2016-10-01 17:02:56 -0300

    first signed commit

--- namshi.sh
+++ namshi.sh
@@ -6,7 +6,7 @@
 
 export github_user="dockerizer"
 export file="test.txt"
-export gitkey="                               "
+export gitkey="bf571c4fd307228749448e893d666ee26e3e56e7"
 
 # load curl with your credentials
 alias curl="curl -H 'Authorization: token $gitkey'"
